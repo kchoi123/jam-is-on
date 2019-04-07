4c094d999f8692fefa798761bc698bd23ae87220
Liam Condon <condonliamt@gmail.com>
2019-04-07 13:37:24 -0700

    edit schema and seeds, started on match algorithm

--- db/schema.sql
+++ db/schema.sql
@@ -9,14 +9,16 @@ CREATE TABLE musician
 (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
+	profile_pic TEXT NOT NULL,
     email VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
-	band BOOLEAN DEFAULT false,
-	primary_instrument VARCHAR(255),
+	music_link TEXT,
+	band BOOLEAN NOT NULL DEFAULT false,
+	primary_instrument VARCHAR(255) NOT NULL,
 	secondary_instrument VARCHAR(255),
-	primay_genre VARCHAR(255),
+	primary_genre VARCHAR(255) NOT NULL,
 	secondary_genre VARCHAR(255),
-	schedule VARCHAR(255),
+	availability VARCHAR(255),
 	PRIMARY KEY (id)
 );
 
@@ -24,14 +26,15 @@ CREATE TABLE band
 (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
+	profile_pic TEXT NOT NULL,
     email VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
-	musician BOOLEAN DEFAULT false,
-	primary_instrument VARCHAR(255),
+	music_link TEXT,
+	musician BOOLEAN NOT NULL DEFAULT false,
+	primary_instrument VARCHAR (255),
 	secondary_instrument VARCHAR(255),
-	primay_genre VARCHAR(255),
+	primary_genre VARCHAR(255) NOT NULL,
 	secondary_genre VARCHAR(255),
-	genre VARCHAR(255),
-	schedule VARCHAR(255),
+	availability VARCHAR(255),
 	PRIMARY KEY (id)
 );

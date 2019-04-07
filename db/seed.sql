fee8dc5d2f7f946f4d0e180c73ef00881afc40be
Liam Condon <condonliamt@gmail.com>
2019-04-07 11:56:23 -0700

    edit schema

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
-INSERT INTO musician (name, email, location, band, instruments, genre, schedule) 
+INSERT INTO musician (name, email, location, band, primary_instrument, secondary_instrument, primary_genre, secondary_genre, schedule) 
 VALUES ('Kevin', 'kevin@kchoi.co', 'San Francisco', true, 
 JSON_ARRAY_INSERT(["guitar", "violin", "steelpan"]), JSON_ARRAY_INSERT(["house", "pop", "hip hop", "reggae", "dubstep"]), JSON_ARRAY_INSERT(["thursday", "friday", "saturday", "sunday", "monday"]));
 
-INSERT INTO band (name, email, location, musician, instruments, genre, schedule) 
+INSERT INTO band (name, email, location, musician, primary_instrument, secondary_instrument, primary_genre, secondary_genre, schedule) 
 VALUES ('Kevin', 'kevin@kchoi.co', 'San Francisco', true, 
 JSON_ARRAY_INSERT(["guitar", "violin", "steelpan"]), JSON_ARRAY_INSERT(["house", "pop", "hip hop", "reggae", "dubstep"]), JSON_ARRAY_INSERT(["thursday", "friday", "saturday", "sunday", "monday"]));

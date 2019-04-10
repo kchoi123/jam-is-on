18476aacefb786b42fa6f0ba018562773f98023a
Liam Condon <condonliamt@gmail.com>
2019-04-09 21:13:15 -0700

    updated schema and seeds; created new seeders folder and files for CLI interface; updated package.json with mysql2 as a dependancy; updated bands and musicians models; DB queries are workinggit reset HEAD routes/musician-api-routes.js

---
+++ seeders/20190409225505-demo-user.js
@@ -0,0 +1,226 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('bands', [{
+
+    name: 'The Skull Cats',
+    profile_pic: '#',
+    email: 'katz@123.com',
+    password: 'password',
+    location: 'San Francisco',
+    music_link: '#',
+    on_lookout: true, 
+    primary_instrument: "vocals",
+    secondary_instrument: "piano",
+    primary_genre: "punk",
+    secondary_genre: "blues", 
+    availability: "weekdays",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'The Bad Habits',
+    profile_pic: '#',
+    email: 'habits@123.com',
+    password: 'password',
+    location: 'San Francisco',
+    music_link: '#',
+    on_lookout: true, 
+    primary_instrument: "drums",
+    secondary_instrument: "piano",
+    primary_genre: "jazz",
+    secondary_genre: "blues", 
+    availability: "weekdays",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+{
+  name: 'Stannis the Mantis',
+  profile_pic: '#',
+  email: 'mantis@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "guitar",
+  secondary_instrument: "drums",
+  primary_genre: "indie",
+  secondary_genre: "folk", 
+  availability: "weekdays",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Farley',
+  profile_pic: '#',
+  email: 'farley@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: false, 
+  primary_genre: "metal",
+  secondary_genre: "rock", 
+  availability: "weekdays",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Bacon',
+  profile_pic: '#',
+  email: 'bacon@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: false, 
+  primary_genre: "rock",
+  secondary_genre: "jazz", 
+  availability: "weekdays",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Chicken',
+  profile_pic: '#',
+  email: 'chicken@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: false, 
+  primary_genre: "soul",
+  secondary_genre: "folk", 
+  availability: "weekends",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Silver Daggers',
+  profile_pic: '#',
+  email: 'daggers@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "piano",
+  primary_genre: "soul",
+  secondary_genre: "funk", 
+  availability: "weekends",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Loose Change',
+  profile_pic: '#',
+  email: 'loosechange@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "guitar",
+  primary_genre: "pop",
+  availability: "weekends",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'ROFLcopter',
+  profile_pic: '#',
+  email: 'rofl@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "bass",
+  primary_genre: "hip-hop",
+  secondary_genre: "R&B",
+  availability: "weekends",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Red Pony',
+  profile_pic: '#',
+  email: 'pony@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "guitar",
+  secondary_instrument: "piano",
+  primary_genre: "folk",
+  availability: "weekdays",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Backseat Bandits',
+  profile_pic: '#',
+  email: 'bandits@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "guitar",
+  secondary_instrument: "piano",
+  primary_genre: "folk",
+  secondary_genre: "pop",
+  availability: "weekdays",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: "Timbuk and the TuTu's",
+  profile_pic: '#',
+  email: 'tutus@123.com',
+  password: 'password',
+  location: 'San Francisco',
+  music_link: '#',
+  on_lookout: true, 
+  primary_instrument: "guitar",
+  secondary_instrument: "bass",
+  primary_genre: "folk",
+  secondary_genre: "rock",
+  availability: "weekdays",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet sollicitudin sem, sed ullamcorper elit. Duis ultrices laoreet mauris, ac imperdiet nibh blandit nec. Cras ultricies dictum ullamcorper. Nulla blandit pretium nunc eget lacinia. Vivamus ac erat venenatis, pulvinar orci eu, accumsan quam. Nulla vitae pretium velit. Donec in turpis urna. Aliquam ultricies posuere est, vulputate faucibus risus ornare sed. In consectetur sed risus sit amet volutpat. In mauris ante, commodo iaculis feugiat sed, cursus sed nibh. Vestibulum sodales a orci sit amet scelerisque. Aliquam leo magna, suscipit id molestie eu, vehicula tincidunt dui. Vivamus ac molestie mauris. Integer eget condimentum nisl. Ut quam est, tempor at ultrices vitae, vulputate vel ligula.",
+  createdAt: new Date(),
+  updatedAt: new Date()
+}], {});
+
+},
+
+
+
+     
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};

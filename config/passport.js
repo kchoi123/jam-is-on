795b17e686c3767910a3a3bb9d5fe510f5d432e3
kchoi123 <k3vinch0i@gmail.com>
2019-04-10 16:19:02 -0700

    updating server js

--- config/passport.js
+++ config/passport.js
@@ -1,53 +1,34 @@
-// This file contains our passport strategies. We will use the band and musician
-// models and passport.
+// This file contains our passport strategies. We will use the user model and passport.
 
 // needed to secure passwords
 var bCrypt = require('bcrypt-nodejs');
 
-module.exports = function(passport, musico, banda) {
-    // Initialize the passport-local strategy, and musician and band models to be
-    // passed as argument.
-    var Musico = musico;
-    var Banda = banda;
-    var musician = require('passport-local').Strategy;
-    var band = require('passport-local').Strategy;
+module.exports = function(passport, user) {
+    // Inside this block, we initialize the passport-local strategy, and the user model, which will be passed as an argument.
+    var User = user;
+    var LocalStrategy = require('passport-local').Strategy;
 
-    // Then we define our custom strategy with our instance of the musician
-    // like this:
-    passport.use('local-signup', new musician(
 
+    // Then we define our custom strategy with our instance of the LocalStrategy like this:
+    passport.use('local-signup', new LocalStrategy(
+ 
         {
-        name: 'name',
-        profile_pic: 'profile_pic',
-        email: 'email',
-        password: 'password',
-        location: 'location',
-        music_link: 'music_link',
-        band: 'band',
-        primary_instrument: 'primary_instrument',
-        secondary_instrument: 'secondary_instrument',
-        primary_genre: 'primary_genre',
-        secondary_genre: 'secondary_genre',
-
-        // allows us to pass the entire request to the callback, useful for
-        // signing up.
-        passReqToCallback: true
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass the entire request to the callback, which is particularly useful for signing up.
         },
-
+    
         // Callback function to handle storing user's details
         function(req, email, password, done) {
-            // First, add hashed password generating function inside the callback
-            // function.
+            // First, we add our hashed password generating function inside the callback function.
             var generateHash = function(password) {
-
+    
                 return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-
+            
             };
-
-            // Using Sequelize  model we initialized earlier as
-            // User, we check to see if the user already exists, and if not we
-            // add them.
-            Musico.findOne({
+            
+            // Then, using the Sequelize user model we initialized earlier as User, we check to see if the user already exists, and if not we add them.
+            User.findOne({
                 where: {
                     email: email
                 }
@@ -58,30 +39,25 @@ module.exports = function(passport, musico, banda) {
                     });
                 } else {
                     var userPassword = generateHash(password);
-                    // Below, notice that the values in the data object are
-                    // gotten from the req.body object which contains the input
-                    // from our signup form.
+                    // Below, notice that the values in the data object are gotten from the req.body object which contains the input from our signup form. 
                     var data =
                         {
                             email: email,
-
+            
                             password: userPassword,
-
+            
                             firstName: req.body.firstname,
-
+            
                             lastName: req.body.lastname
-
+            
                         };
-
-                    // User.create() is a Sequelize method for adding new entries
-                    // to the database. Notice that the values in the data object
-                    // are gotten from the req.body object which contains the
-                    // input from our signup form.
+                        
+                    // User.create() is a Sequelize method for adding new entries to the database. Notice that the values in the data object are gotten from the req.body object which contains the input from our signup form. 
                     User.create(data).then(function(newUser, created) {
                         if (!newUser) {
                             return done(null, false);
                         }
-
+            
                         if (newUser) {
                             return done(null, newUser);
                         }
@@ -96,12 +72,11 @@ module.exports = function(passport, musico, banda) {
     done(null, user.id);
     });
 
-    // deserialize user
+    // deserialize user 
     passport.deserializeUser(function(id, done) {
-
+ 
         User.findById(id).then(function(user) { // Gets the user
-            if (user) { // If successful, an instance of the sequelize model is
-            // returned
+            if (user) { // If successful, an instance of the sequelize model is returned
             done(null, user.get());
             } else {
             done(user.errors, null);
@@ -110,75 +85,55 @@ module.exports = function(passport, musico, banda) {
     });
 
     //LOCAL SIGNIN
-    passport.use('local-signin', new musician(
-
+    passport.use('local-signin', new LocalStrategy(
+ 
     {
-        // by default, local strategy uses username and password, we will
-        // override with email
+        // by default, local strategy uses username and password, we will override with email
         usernameField: 'email',
-
+ 
         passwordField: 'password',
-
-        passReqToCallback: true // allows us to pass back the entire request to
-        // the callback, which is particularly useful for signing up.
-
+ 
+        passReqToCallback: true // allows us to pass back the entire request to the callback, which is particularly useful for signing up.
+ 
     },
-
+ 
     function(req, email, password, done) {
         var User = user;
-
-        // The isValidPassword function compares the password entered with the
-        // bCrypt comparison method since we stored our password with bcrypt. If
-        // details are correct, user will sign in
+        
+        // The isValidPassword function compares the password entered with the bCrypt comparison method since we stored our password with bcrypt. If details are correct, user will sign in
         var isValidPassword = function(userpass, password) {
             return bCrypt.compareSync(password, userpass);
         }
-
-        Banda.findOne({
+ 
+        User.findOne({
             where: {
                 email: email
             }
         }).then(function(user) {
-
+ 
             if (!user) {
-                Musico.findOne({
-                    where: {
-                        email: email
-                    }
-                }).then(function(user2) {
-                    if (!user2) {
-                        return done(null, false, {
-                            message: 'Email does not exist'
-                        });
-                    }
-                    if (!isValidPassword(user2.password, password)) {
-                        return done(null, false, {
-                            message: 'Incorrect password.'
-                        });
-                    }
-                    var userinfo = user2.get();
-                    return done(null, userinfo);
-                }).catch(function(err) {
-                    console.log("Error:", err);
-                    return done(null, false, {
-                        message: 'Something went wrong with your Signin'
-                    });
+                return done(null, false, {
+                    message: 'Email does not exist'
                 });
+ 
             }
-
+ 
             if (!isValidPassword(user.password, password)) {
                 return done(null, false, {
                     message: 'Incorrect password.'
                 });
+ 
             }
+ 
+ 
             var userinfo = user.get();
             return done(null, userinfo);
-
-
+ 
+ 
         }).catch(function(err) {
-
+ 
             console.log("Error:", err);
-
+ 
             return done(null, false, {
                 message: 'Something went wrong with your Signin'
             });

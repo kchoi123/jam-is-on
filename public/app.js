e7f5270948991d784264c4ea5fd4055f0361ad24
lalitha <lalithapelluri6@gmail.com>
2019-04-11 14:33:10 -0700

    click listener to signup new user

--- public/app.js
+++ public/app.js
@@ -35,6 +35,61 @@ $(function() {
         })
     });
 
-});
+    // New user for BAND
+    $("#newUser").on("submit", function (event) {
+        event.preventDefault();
 
-//selecting options in sign-up
+        var newUser = {
+            name: $("#firstName").val().trim(),
+            profile_pic: $("#lastName").val().trim(),
+            email: $("#username").val().trim(),
+            password: $("#password").val().trim(),
+            location: $("#firstName").val().trim(),
+            music_link: $("#lastName").val().trim(),
+            musician: $("#username").val().trim(),
+            primary_instrument: $("#password").val().trim(),
+            secondary_instrument: $("#firstName").val().trim(),
+            primary_genre: $("#lastName").val().trim(),
+            secondary_genre: $("#username").val().trim()
+        }
+
+        $.post("/signUpB", newUser).then(function (res) {
+            $("#newUserMessage").text(res);
+        })
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+    });
+
+    $('.sidenav').sidenav();
+    // $("#createAccount").on("click", function(e) {
+    //     e.preventDefault(); 
+    //     var $form = $("#updateForm");  
+    // $form.append("<input type='hidden' name='link_redirect' value='" + this.href + "'>");  // This is added to the end of the form and triggers the redirect after the saving is done
+    // $form.submit(); // Submitting the form saves the data, and then redirects
+    // });
+   
+    $("#createAccount").on("click", function(e) {
+        e.preventDefault();
+
+        var newUser = {
+            name: $("#firstName").val().trim(),
+            profile_pic: $("#lastName").val().trim(),
+            email: $("#username").val().trim(),
+            password: $("#password").val().trim(),
+            location: $("#firstName").val().trim(),
+            music_link: $("#lastName").val().trim(),
+            musician: $("#username").val().trim(),
+            primary_instrument: $("#password").val().trim(),
+            secondary_instrument: $("#firstName").val().trim(),
+            primary_genre: $("#lastName").val().trim(),
+            secondary_genre: $("#username").val().trim()
+        }
+
+        $.post("/signup", newUser).then(function (res) {
+           
+        });
+    });
+
+});

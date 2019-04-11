3234f73bb541240a1745a29a75b2827b31150f60
kchoi123 <k3vinch0i@gmail.com>
2019-04-11 11:53:36 -0700

    update server.js for passport ready and add new flash package for npm install.

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var passport = require('passport');
 var session = require('express-session');
+var flash = require('connect-flash');
+var session = require('express-session');
+var db = require("./models");
 // var env = require('dotenv').load()
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -24,15 +23,33 @@ app.use(express.json());
 
 // Initialize passport, express session and passport session.
 app.use(session({
-  secret: 'keyboard cat',
+  key: 'user_sid',
+  secret: 'goN6DJJC6E287cC77kkdYuNuAyWnz7Q3iZj8',
   resave: true,
-  saveUninitialized: true
-})); // session secret
+  saveUninitialized: false,
+  cookie:
+  {
+    expires: 600000,
+    httpOnly: false
+  }
+}));
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 // Static directory
 app.use(express.static("public"));
+
+// Connect flash
+app.use(flash());
+
+// Global variables for flash
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -40,17 +57,19 @@ app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
-// require("./routes/html-routes.js")(app);
-require("./routes/band-api-routes.js")(app,passport);
-require("./routes/musician-api-routes.js")(app,passport);
-
+// load bandapiroutes not being used yet
+require("./routes/band-api-routes.js")(app, passport);
+// load musicianapiroutes
+require("./routes/musician-api-routes.js")(app, passport);
 // load passport strategies
 require('./config/passport.js')(passport, db.Musician);
 
+// Setup server information
+var PORT = process.env.PORT || 8080;
+
 // Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });

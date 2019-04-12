b968f41d7dcf7f2f0d589e205efb56a5948a89c2
kchoi123 <k3vinch0i@gmail.com>
2019-04-12 10:41:45 -0700

    making signup sheet work with our project

--- models/musician.js
+++ models/musician.js
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: true,
             validate: {
-                len: [1,20]
+                len: [1]
             }
         },
         location: {
